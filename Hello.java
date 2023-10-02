29e7ed49ac1f2294dce84caa24ef4d4c057eda25
Aryan Khan <106798607+TOG-Marshal@users.noreply.github.com>
2023-10-02 09:48:08 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+class MySwing{
+    public static void main(String[] args) {
+        JFrame page =new JFrame("HOSPITAL MANAGEMENT SYSTEM");
+        page.setSize(500,500);
+        page.setLocation(0, 0);
+        page.setVisible(true);
+    }
+}
